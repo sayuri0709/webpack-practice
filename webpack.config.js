cb7f0ec17f0b6eaa00400f342f2bd148390e3845
sayuri.hamada <sayuri.hamada@gpol.co.jp>
2021-04-02 11:09:27 +0900

    ES6, React, Vue etc...

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,11 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
   entry: './src/javascripts/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -12,13 +15,40 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.vue/,
+        exclude: /node_modules/, // node_modules内のファイルを除外
+        use: [
+          {
+            loader: 'vue-loader'
+          }
+        ]
+      },
+      {
+        test: /\.js/,
+        exclude: /node_modules/,　// node_modules内のファイルを除外
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { 'targets': '> 0.25%, not dead' }],
+                '@babel/preset-react'
+              ]
+            }
+          }
+        ]
+      },
+      {
         test: /\.(css|sass|scss)/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader
           },
           {
-            loader: 'css-loader'
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
           },
           {
             loader: 'sass-loader'
@@ -26,11 +56,22 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|jpg)/, //正規表現
+        test: /\.(png|jpg|jpeg)/, //正規表現
         type: 'asset/resource',
         generator: {
           filename: 'images/[name][ext]',
-        }
+        },
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              }
+            }
+          }
+        ]
       },
       {
         test: /\.pug/,
@@ -49,6 +90,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
       filename: './stylesheets/my.css'
     }),
